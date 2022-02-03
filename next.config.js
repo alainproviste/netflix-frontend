85a64bb7bda430fbb37189f18e2479b614629a11
alainproviste <remikarmann@hotmail.com>
2022-02-03 10:22:10 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    API_URL: process.env.API_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_STRIPE_PK: process.env.NEXT_PUBLIC_STRIPE_PK
+  }
 }

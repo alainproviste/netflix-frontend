ca2b83a47a68edec7e91e3cb0c8a32ea11b42f5b
Rémi KARMANN <remikarmann@hotmail.com>
2022-03-01 16:24:53 +0100

    correction

--- src/services/stripe.service.js
+++ src/services/stripe.service.js
@@ -1,6 +1,8 @@
+import apiConfigs from "../../next.config.js";
+
 export default {
     createSession(token, body) {
-        return fetch("http://localhost:3131/api/v1/checkout", {
+        return fetch(`${apiConfigs.env.API_URL}api/v1/checkout`, {
             method: "POST",
             headers: {
                 "content-type": "application/json",
